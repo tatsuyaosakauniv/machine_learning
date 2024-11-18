740f0eecb727fa0e1ff0aee39b6e62f958be0817
tatsuyaosakauniv <u784868c@ecs.osaka-u.ac.jp>
2024-11-19 02:16:16 +0900

    プッシュテスト1

--- src/jupyter_textbook/list_7-2-1.py
+++ src/jupyter_textbook/list_7-2-1.py
@@ -5,3 +5,14 @@ import time
 
 np.random.seed(1) # (A)
 import keras.optimizers # (B)
+from keras.models import Sequential # (C)
+from keras.layers.core import Dense. Activation # (D)
+
+# データのload
+outfile = np.load('class_data.npz')
+X_train = outfile['X_train']
+T_train = outfile['T_train']
+X_test = outfile['X_test']
+T_test = outfile['T_test']
+X_range0 = outfile['X_range0']
+X_range1 = outfile['X_range1']
